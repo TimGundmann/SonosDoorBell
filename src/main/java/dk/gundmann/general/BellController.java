51ad325432fcd6d29339a865f7a2e514ec6f8868
Tim Gundmann <tim@gundmann.dk>
2016-01-30 20:41:05 +0100

    Logging added

--- src/main/java/dk/gundmann/general/BellController.java
+++ src/main/java/dk/gundmann/general/BellController.java
@@ -1,14 +1,18 @@
 package dk.gundmann.general;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BellController {
     
     @RequestMapping("/ringbell")
     public String index() {
-        return "Greetings from Spring Boot!";
+        System.out.println("Call to door bell at " + DateTimeFormatter.ofPattern("yyyy/MM/dd HHS:mm:ss z").format(ZonedDateTime.now()));    	
+        return "Sons bell is ringing!";
     }
     
 }
