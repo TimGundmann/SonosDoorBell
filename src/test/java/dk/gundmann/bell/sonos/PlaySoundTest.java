d20583fec5b4df0b4cdce9fed073453e2e515e6c
Tim Gundmann <tim@gundmann.dk>
2016-02-12 16:16:50 +0100

    Test fixed and more test added

--- src/test/java/dk/gundmann/bell/sonos/PlaySoundTest.java
+++ src/test/java/dk/gundmann/bell/sonos/PlaySoundTest.java
@@ -1,25 +1,33 @@
 package dk.gundmann.bell.sonos;
 
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.internal.verification.VerificationModeFactory;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.tensin.sonos.commander.Sonos;
 import org.tensin.sonos.control.ZonePlayer;
 import org.tensin.sonos.gen.AVTransport.GetPositionInfoResponse;
-@Ignore
+
 @RunWith(MockitoJUnitRunner.class)
 public class PlaySoundTest {
 
 	private final static String SOUND = "SOUND";
+
+	private static final int TRACK_NUMBER = 2;
 	
 	@Mock
 	private Sonos sonos;
@@ -27,6 +35,9 @@ public class PlaySoundTest {
 	@Mock
 	private GetPositionInfoResponse currentSongInfo;
 	
+	private ZonePlayer player = mock(ZonePlayer.class);
+
+	@Mock
 	private PlaySound playSound;
 
 	@Before
@@ -38,27 +49,43 @@ public class PlaySoundTest {
 	
 	@Test
 	public void givenAPlayerWillPauseTheCurrentSong() throws Exception {
-		// given
-		ZonePlayer player = mock(ZonePlayer.class);
-
-		// when
+		// given when
 		playSound.play(player);
 
 		// then
-		verify(sonos).currentSongPosition(player);
+		verify(sonos, atLeast(1)).currentSongPosition(player);
 	}
 	
 	@Test
 	public void verifyThatTheBellSoundIsPlayed() throws Exception {
-		// given
-		ZonePlayer player = mock(ZonePlayer.class);
-
-		// when
+		// given when
 		playSound.play(player);
 
 		// then
 		verify(sonos).play(eq(player), eq(SOUND));
-		verify(sonos).play(eq(player));
+		verify(sonos, atLeast(1)).play(eq(player));
 	}
 	
+	@Test
+	public void verifyThatTheBellTrackIsRemoved() throws Exception {
+		// given when
+		playSound.play(player);
+
+		// then
+		verify(sonos).remove(eq(player), anyInt());		
+	}
+	
+	@Test
+	public void verifyThatTheCurrentSongPlayingIsRestarted() throws Exception {
+		// given 
+		when(currentSongInfo.track()).thenReturn(TRACK_NUMBER);
+		
+		// when
+		playSound.play(player);
+
+		// then
+		verify(sonos).play(eq(player), eq(TRACK_NUMBER));
+		verify(sonos).forward(eq(player), anyString());
+		verify(sonos, atLeast(1)).play(eq(player));
+	}
 }
