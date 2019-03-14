c8c552246559b6dc553fad5348aeb0d71923de8e
Tim Gundmann <tim@gundmann.dk>
2019-03-14 16:55:12 +0100

    roll back of version update

--- src/main/java/dk/gundmann/general/SonosUpnpServiceConfiguration.java
+++ src/main/java/dk/gundmann/general/SonosUpnpServiceConfiguration.java
@@ -1,8 +1,8 @@
 package dk.gundmann.general;
 
 import org.fourthline.cling.DefaultUpnpServiceConfiguration;
-import org.fourthline.cling.transport.impl.StreamClientConfigurationImpl;
-import org.fourthline.cling.transport.impl.StreamClientImpl;
+import org.fourthline.cling.transport.impl.apache.StreamClientConfigurationImpl;
+import org.fourthline.cling.transport.impl.apache.StreamClientImpl;
 import org.fourthline.cling.transport.spi.StreamClient;
 
 public class SonosUpnpServiceConfiguration extends DefaultUpnpServiceConfiguration {
