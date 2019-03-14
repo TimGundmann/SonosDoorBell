2ed1450f80482c9b0991748003d5dcd6b2f9e36e
Tim Gundmann <tim@gundmann.dk>
2019-03-14 15:59:04 +0100

    logger clean up

--- src/main/java/dk/gundmann/general/SonosController.java
+++ src/main/java/dk/gundmann/general/SonosController.java
@@ -1,18 +1,15 @@
 package dk.gundmann.general;
 
-import java.io.IOException;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
